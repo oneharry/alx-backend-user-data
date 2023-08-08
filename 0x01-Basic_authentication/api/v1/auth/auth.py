e1ac3719547677debf774c97fc73ac47784e22af
oneharry <harryofficialchidi@yahoo.com>
2023-08-08 18:57:32 +0000

    debug auth methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,9 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns bool"""
         slashed_path = path if path.endswith('/') else path + '/'
-        if path is not None and slashed_path in excluded_paths:
+        if slashed_path is not None and slashed_path in excluded_paths:
             return False
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if slashed_path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
     def authorization_header(self, request=None) -> str:
