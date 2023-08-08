01b7af875d3b40dbd98cd17bff1dc29517877639
oneharry <harryofficialchidi@yahoo.com>
2023-08-08 19:04:43 +0000

    debug auth methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,10 +18,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Returns header obj"""
-        if request is None or request.headers.get("Authorization") is None:
-            return request
+        if request is None or "Authorization" not in request.headers:
+            return None
         return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns user obj"""
-        return request
+        return None
