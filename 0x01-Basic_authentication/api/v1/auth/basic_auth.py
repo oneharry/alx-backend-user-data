1c9e415ba861f9b4cf4ae401844353600d7c8438
oneharry <harryofficialchidi@yahoo.com>
2023-08-13 22:45:04 +0000

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,12 +53,14 @@ class BasicAuth(Auth):
         if user_pwd is None or type(user_pwd) is not str:
             return None
         try:
-            user = User.search({'email': user_email})
-
+            all_users = User.search({'email': user_email})
         except Exception:
             return None
-        if user.is_valid_password(user_pwd):
-            return user
+
+        for user in all_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
