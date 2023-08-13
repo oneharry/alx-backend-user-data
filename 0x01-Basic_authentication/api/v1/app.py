58cc883816ef8d4cbf9a3b3fb49439db59cddd08
oneharry <harryofficialchidi@yahoo.com>
2023-08-13 17:56:02 +0000

    fixed task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -37,7 +37,7 @@ def before_request() -> str:
     if auth.require_auth(request.path, request_list):
         if auth.authorization_header(request) is None:
             abort(401)
-        if auth.current_user(request):
+        if auth.current_user(request) is None:
             abort(403)
 
 
