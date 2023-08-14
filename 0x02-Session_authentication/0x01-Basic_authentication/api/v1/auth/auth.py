e51050595c5cd2bdda4044375754dfc6affa8b73
oneharry <harryofficialchidi@yahoo.com>
2023-08-14 08:02:34 +0000

    task1

---
+++ 0x02-Session_authentication/0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Authentication class """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns bool"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path if path.endswith('/') else path + '/'
+
+        for pa in excluded_paths:
+            if pa.endswith('*'):
+                temp = path[:-1]
+                if path.startswith(temp):
+                    return False
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Returns header obj"""
+        if request is None or "Authorization" not in request.headers:
+            return None
+        header = request.headers.get("Authorization")
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns user obj"""
+        return None
--- 0x02-Session_authentication/0x01-Basic_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/0x01-Basic_authentication/api/v1/views/users.py
@@ -27,10 +27,6 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id is 'me' and request.current_user is None:
-        abort(404)
-    if user_id is 'me' and request.current_user:
-        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -124,13 +120,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
-
-@app_views.route('/users/<user_id>', methods=['UPDATE'], strict_slashes=False)
-def update_users(user_id: str) -> str:
-    """update user """
-    user = User.get(user_id)
-    if user is 'me' and request.current_user is None or user_id is None:
-        abort(404)
-    if user is 'me' and request.current_user:
-        return jsonify(user.to_json())
