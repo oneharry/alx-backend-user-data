38a2969761f6d2a16aeda402a921802238612933
oneharry <harryofficialchidi@yahoo.com>
2023-08-17 18:25:29 +0000

    Task 1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,5 +13,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
-    session_is = Column(String)
+    session_id = Column(String)
     reset_token = Column(String)
