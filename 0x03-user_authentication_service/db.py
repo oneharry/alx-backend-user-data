2af7c1c9dc981324b35e076ad45202e805e41a39
oneharry <harryofficialchidi@yahoo.com>
2023-08-17 18:47:12 +0000

    Task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -35,3 +37,13 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kw) -> User:
+        """Returns the first row of users matching kwargs"""
+        try:
+            return self._session.query(User).filter_by(**kw).first()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+
