c8b4854f8b99d4f2eedda24862af127f0fb4afed
oneharry <harryofficialchidi@yahoo.com>
2023-08-17 19:59:54 +0000

    hashing password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """hash the password"""
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_pwd
